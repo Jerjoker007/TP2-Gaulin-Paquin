7b7e5dfb9063bb82c6e1dc83a032d2370f1ae163
Jeremie-Paquin <paquin.jeremi@gmail.com>
2026-03-30 14:24:40 -0400

    first commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\AuthController;
+
+
+
+
